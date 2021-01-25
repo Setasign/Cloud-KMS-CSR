be9f74fb656ae2a4089e636480ab12abad84e307
Jan Slabon <jan.slabon@setasign.com>
2021-01-25 14:22:07 +0100

    Added copyright header

--- src/Csr.php
+++ src/Csr.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * @copyright Copyright (c) 2021 Setasign GmbH & Co. KG (https://www.setasign.com)
+ * @license   http://opensource.org/licenses/mit-license The MIT License
+ */
+
 namespace setasign\CloudKmsCsr;
 
 use SetaPDF_Signer_Pem as Pem;
--- src/GoogleCloudKMS/Updater.php
+++ src/GoogleCloudKMS/Updater.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * @copyright Copyright (c) 2021 Setasign GmbH & Co. KG (https://www.setasign.com)
+ * @license   http://opensource.org/licenses/mit-license The MIT License
+ */
+
 namespace setasign\CloudKmsCsr\GoogleCloudKMS;
 
 use setasign\CloudKmsCsr\Exception;
